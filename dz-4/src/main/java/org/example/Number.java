dc0f5f429a5512f18f01c14b4cb164297a07d4b7
oksanafrasynuyk <ofr@pinpuuls.com>
2024-05-20 23:21:57 +0300

    dz4

---
+++ dz-4/src/main/java/org/example/Number.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class  Number {
+    int num=100;
+
+    public int getNum() {
+        System.out.printf("---%s---\n", this.num);
+        return num;
+    }
+}
---
+++ dz-4/src/main/java/org/example/Phone.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Phone {
+    String phoneBrand = "Iphone";
+    int price = 1200;
+    char currency = '$';
+
+   public Phone(String phoneBrand, int price, char currency) {
+        System.out.printf("phone1 %s has price %d %s \n", this.phoneBrand, this.price, this.currency);
+        this.phoneBrand = phoneBrand;
+        this.price = price;
+        this.currency = currency;
+    }
+
+}
