b5239aba183df01ddaa89fbbae5730898620bd2e
oksanafrasynuyk <ofr@pinpuuls.com>
2024-05-20 23:24:31 +0300

    dz4

---
+++ dz-4/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+//        просто розбиралась
+//        Phone iphone = new Phone("samsung", 900, '$');
+//        System.out.printf("phone %s has price %d %s \n", iphone.phoneBrand, iphone.price, iphone.currency);
+    Number num = new Number();
+        System.out.printf("~~~%s~~~\n",num.getNum());
+    }
+}
